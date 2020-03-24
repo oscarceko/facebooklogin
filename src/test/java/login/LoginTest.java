365829d24f4c0f554c7550284f1f3368dc70a429
oscarceko <62601481+oscarceko@users.noreply.github.com>
2020-03-24 14:33:46 -0700

    push first code commit

---
+++ src/test/java/login/LoginTest.java
@@ -0,0 +1,37 @@
+package login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    private WebDriver driver;
+
+    public void setup() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://www.facebook.com/");
+        Thread.sleep(5000);
+
+        //1. maximize the window
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+        driver.findElement(By.id("email")).sendKeys("08136148449");
+        driver.findElement(By.id("pass")).sendKeys("819967");
+        driver.findElement(By.id("loginbutton")).click();
+
+    }
+    public static void main(String args[]) throws InterruptedException {
+        LoginTest test = new LoginTest();
+                test.setup();
+
+    }
+}
+
+
+
+
